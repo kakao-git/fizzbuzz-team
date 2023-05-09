f930353508f5515c1715323091271a22acaad6ba
chaemina <chaemina82@naver.com>
2023-05-10 00:15:24 +0900

    feat: Print 'buzz'
    if i is multiple of 5.
    Otherwise print i itself

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,6 +2,9 @@ def do_fizzbuzz(num: int):
     for i in range(1, num+1):
         if i%3 ==0:
             print('fizz')
+        if i%5 ==0:
+            print('buzz')
+
         else:
             print(i)
 
